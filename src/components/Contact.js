be168013920bd2a772c399094d966a228c97da75
Vaibhav Shetty <54659596+VaiBhav029@users.noreply.github.com>
2023-03-24 23:48:08 +0530

    Added Getfrom api in env

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-full  bg-[#0a192f] flex justify-center items-start px-2'>
-        <form method='POST' action='' className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action={process.env.REACT_APP_GETFROM} className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-7'>
                 <p className='text-4xl font-bold border-b-2 inline border-green-500 text-gray-300'>Contact</p>
                 <p className='py-4 text-gray-300'>//Send me a email </p>
